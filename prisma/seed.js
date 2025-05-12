1ae86fe6c5c8b4b3f7601fedc7133f2bf3a36105
fernandomec <fernandomec2013@gmail.com>
2025-05-12 14:09:50 -0300

    bug fix with seed.js

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,28 +1,22 @@
 const { PrismaClient } = require('@prisma/client');
 const bcrypt = require('bcrypt');
+import { users } from './users';
 
 const prisma = new PrismaClient();
 
-async function seed() {
-  try {
-    const hashedPassword = await bcrypt.hash('HFxX8.k%0W%Gs1[0_@Y}', 10);
-
-    const user = await prisma.user.create({
-      data: {
-        nome: 'teste',
-        email: 'admin@gmail.com',
-        password: hashedPassword,
-        role: 'ADMIN',
-        username: 'admin'
-      },
-    });
-
-    console.log('Usuário administrador criado:', user);
-  } catch (error) {
-    console.error('Erro ao criar usuário administrador:', error);
-  } finally {
-    await prisma.$disconnect();
+async function main() {
+  for (let user of users) {
+    await prisma.user.create({
+      data: user
+    })
   }
 }
 
-seed();
+main().catch((e) => {
+  console.error(e);
+  process.exit(1);
+}).finally(async () => {
+  await prisma.$disconnect();
+});
+
+module.exports = main;
---
+++ prisma/users.js
@@ -0,0 +1,6 @@
+export const users = {
+    name:'admin',
+    email:'admin@gmail.com',
+    password:'HFxX8.k%0W%Gs1[0_@Y',
+    role:'admin'
+};
